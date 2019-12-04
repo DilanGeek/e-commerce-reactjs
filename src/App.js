241f91eab5790157bc954e187975e96e158fe7e7
Dilan Atondo <dilan.atondo@g-global.com.mx>
2019-12-04 12:27:29 -0800

    shop sections

--- src/App.js
+++ src/App.js
@@ -1,43 +1,18 @@
 import React from 'react';
-import { Route, Link } from 'react-router-dom';
-
-import HomePage from '../src/pages/homepage/homepage.component'
+import { Route, Switch } from 'react-router-dom';
 
 import './App.css';
 
-// const HomePage = props => {
-//   console.log(props);
-//   return (
-//     <div>
-//       <button onClick={() => props.history.push('/topics')}>Topics </button>
-//       <h1>HOME PAGE</h1>
-//     </div>
-//   );
-// };
-
-// const TopicsList = props => {
-//   return (
-//     <div>
-//       <h1>TOPIC LIST PAGE</h1>
-//       <Link to={`${props.match.url}/13`}>TO TOPIC 13</Link>
-//       <Link to={`${props.match.url}/17`}>TO TOPIC 17</Link>
-//       <Link to={`${props.match.url}/21`}>TO TOPIC 21</Link>
-//     </div>
-//   );
-// };
-
-// const TopicDetail = props => {
-//   return (
-//     <div>
-//       <h1>TOPIC DETAI L PAGE: {props.match.params.topicId}</h1>
-//     </div>
-//   );
-// };
+import HomePage from '../src/pages/homepage/homepage.component'
+import ShopPage from '../src/pages/shop/shop.component'
 
 function App() {
   return (
     <div>
-      <Route exact path='/' component={HomePage} />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/shop' component={ShopPage} />
+      </Switch>
       {/* <Route exact path='/blog/asdqw/topics' component={TopicsList} />
       <Route path='/blog/asdqw/topics/:topicId' component={TopicDetail} />
       <Route exact path='/blog/topics' component={TopicsList} />
