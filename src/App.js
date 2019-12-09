5f491cf51d5472b342b0fed0abdf1c2ccb213243
Dilan Atondo <dilan.atondo@g-global.com.mx>
2019-12-09 11:32:51 -0800

    sing up and save data un firebase

--- src/App.js
+++ src/App.js
@@ -1,32 +1,43 @@
+
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 
 import './App.css';
 
-import Header from './components/header/header.component'
-import HomePage from '../src/pages/homepage/homepage.component'
-import SingInAndSingUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component'
-import ShopPage from '../src/pages/shop/shop.component'
-import { auth } from './firebase/firebase.utils'
+import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+import Header from './components/header/header.component';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
 class App extends React.Component {
-  // function App() {
-
   constructor() {
-    super()
+    super();
 
     this.state = {
       currentUser: null
-    }
+    };
   }
 
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user });
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
 
-      console.log(user);
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          });
+          console.log(this.state)
+        });
+      }
+
+      this.setState({ currentUser: userAuth });
     });
   }
 
@@ -41,9 +52,9 @@ class App extends React.Component {
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
-          <Route path='/signin' component={SingInAndSingUp} />
+          <Route path='/signin' component={SignInAndSignUpPage} />
         </Switch>
-      </div >
+      </div>
     );
   }
 }
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -3,6 +3,38 @@ import 'firebase/firestore'
 import 'firebase/auth'
 
 const config = {
+    apiKey: "AIzaSyCe88Oplkz60qQfvycXKYY8ps34rTgq4wc",
+    authDomain: "crwn-bd-bc3a0.firebaseapp.com",
+    databaseURL: "https://crwn-bd-bc3a0.firebaseio.com",
+    projectId: "crwn-bd-bc3a0",
+    storageBucket: "crwn-bd-bc3a0.appspot.com",
+    messagingSenderId: "289029060127",
+    appId: "1:289029060127:web:97ab7d17749573a4a5072a"
+}
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+    if (!userAuth) return;
+
+    const userRef = firestore.doc(`users/${userAuth.uid}`)
+
+    const snapShot = await userRef.get()
+
+    if (!snapShot.exists) {
+        const { displayName, email } = userAuth;
+        const createdAt = new Date();
+
+        try {
+            await userRef.set({
+                displayName,
+                email,
+                createdAt,
+                ...additionalData
+            })
+        } catch (error) {
+            console.log('error creating user', error.message)
+        }
+    }
+    return userRef;
 
 }
 
