4e312eac2b1928b60def316f7979b41033e44416
Dilan Atondo <dilan.atondo@g-global.com.mx>
2019-12-09 14:57:14 -0800

    sign up fixed

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -12,42 +12,38 @@ const config = {
     appId: "1:289029060127:web:97ab7d17749573a4a5072a"
 }
 
+firebase.initializeApp(config);
+
 export const createUserProfileDocument = async (userAuth, additionalData) => {
     if (!userAuth) return;
 
-    const userRef = firestore.doc(`users/${userAuth.uid}`)
+    const userRef = firestore.doc(`users/${userAuth.uid}`);
 
-    const snapShot = await userRef.get()
+    const snapShot = await userRef.get();
 
     if (!snapShot.exists) {
         const { displayName, email } = userAuth;
         const createdAt = new Date();
-
         try {
             await userRef.set({
                 displayName,
                 email,
                 createdAt,
                 ...additionalData
-            })
+            });
         } catch (error) {
-            console.log('error creating user', error.message)
+            console.log('error creating user', error.message);
         }
     }
+
     return userRef;
-
-}
-
-firebase.initializeApp(config)
+};
 
 export const auth = firebase.auth();
-
 export const firestore = firebase.firestore();
 
-const provider = new firebase.auth.GoogleAuthProvider()
-
+const provider = new firebase.auth.GoogleAuthProvider();
 provider.setCustomParameters({ prompt: 'select_account' });
-
 export const signInWithGoogle = () => auth.signInWithPopup(provider);
 
-export default firebase; 
+export default firebase;
